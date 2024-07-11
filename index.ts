57d0f5c5164414f34453e8a66a880bd6c2eea3c4
manohySr <manohyrajaonah@gmail.com>
2024-07-11 22:31:12 +0400

    1-definition of array in js with bigO notation

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 /*
-DS: Data Structures
+  There is static array and dynamic array
+  In other language like C++ or Java, you have access static array and more
+  access to memory usage
 
-DESCRIPTION
-  DS are a way to structure the data you store in the ram
-
-
-THESE ARE THE MAIN OPERATION WE DO ON A DS
-  - Insertion
-  - Deletion
-  - Traversial
-  - Searching
-  - Sorting
-  - Access
+  But in JS or higher language like python, you don't need to worry about that,
+  it's  dynamic
+  so sometime the 0(1) is O(n) in push method per example
 */
+
+const strings: string[] = ["a", "b", "c"];
+
+strings.push("d"); // O(1)
+
+strings.pop(); // O(1)
+
+strings.unshift("x"); // O(n)
+
+strings.splice(2, 0, "string added"); // O(n/2) => O(n)
+
+console.log(strings);
