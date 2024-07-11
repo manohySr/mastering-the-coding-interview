7984bbd6d92720f7b4b16e36030154aa9b66db20
manohySr <manohyrajaonah@gmail.com>
2024-07-11 21:10:36 +0400

    fix version

--- index.ts
+++ index.ts
@@ -1,15 +1,51 @@
-/*
-DS: Data Structures
+// how to solve problem
 
-DESCRIPTION
-  DS are a way to structure the data you store in the ram
+/**
+ * Given 2 arrays - no size
+ * return true or false
+ *
+ * the brute force solution is O(n^2)
+ */
+// data
+const array1: string[] = ["a", "b", "c"];
+const array2: string[] = ["p", "e", "f", "x"];
 
+// brute force solution: O(n^2)
+// function containsCommonItem(arr1: string[], arr2: string[]): boolean {
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         return true;
+//       }
+//     }
+//   }
 
-THESE ARE THE MAIN OPERATION WE DO ON A DS
-  - Insertion
-  - Deletion
-  - Traversial
-  - Searching
-  - Sorting
-  - Access
-*/
+//   return false;
+// }
+// O(a*b)
+
+// Efficient solution
+function containsCommonItem(arr1: string[], arr2: string[]): boolean {
+  /*
+  Loop through the first array and create an object with true property
+  Loop through the second array and check arr2[index] == obj.property
+  */
+  const map: { [key: string]: boolean } = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+    const item = arr1[i]
+    if (!map[item]) {
+      map[item] = true;
+    }
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(containsCommonItem(array1, array2));
