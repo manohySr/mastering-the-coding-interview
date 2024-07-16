f6b23560368c8ceabbd59d801b96ba4c7b07527a
manohySr <manohyrajaonah@gmail.com>
2024-07-16 21:52:37 +0400

    add: definition of data structures

---
+++ 1-ds-action/index.ts
@@ -0,0 +1,15 @@
+/*
+DS: Data Structures
+
+DESCRIPTION
+  DS are a way to structure the data you store in the ram
+
+
+THESE ARE THE MAIN OPERATION WE DO ON A DS
+  - Insertion
+  - Deletion
+  - Traversial
+  - Searching
+  - Sorting
+  - Access
+*/
