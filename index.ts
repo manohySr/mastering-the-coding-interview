567ab98bb4646957f03a2a6b2e7300f5904aec20
manohySr <manohyrajaonah@gmail.com>
2024-07-18 22:12:55 +0400

    Hash tables data structure

---
+++ index.ts
@@ -0,0 +1,19 @@
+/*
+Hash tables
+    Dictionnary in python, Object in Js, Maps in Java
+    key value pair
+    hash function (optimum): 
+        - input => hash(input)
+        - same input same result
+        - this is idempotent
+    used in cache system
+
+    context:
+        we give a key that we hash using the hash function
+        and a data to store
+        when we add, remove, read we should always give the key
+
+    insert O(1) - lookup O(1) - delete O(1) - search O(1)
+
+    cons: when we have a collision the performance can be O(n) when lookup
+*/
