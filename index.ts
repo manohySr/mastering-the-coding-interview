0e3c2b5501e0bb9c76e85d9c22ee5a929b0dd84c
manohySr <manohyrajaonah@gmail.com>
2024-07-25 21:54:21 +0400

    add: description linkedlist DS

---
+++ index.ts
@@ -0,0 +1,22 @@
+/**
+ * Linked Lists
+ * keywords:
+ *      head - tail - null
+ * It is a linked lists (lol)
+ * TS/JS doesn't have linkedlist DS prebuild in (Java has a linkedlist prebuild)
+ * the list contains node:
+ *      a node has a data and a pointer
+ *          data is the actual data
+ *          pointer point to the next data
+ *          exemple:
+ *              apple -> banana -> ananas -> null
+ *              but more like
+ *              apple
+ *              10058 -> banana
+ *                       89603 -> ananas
+ *                                45213 -> null
+ *          we can take a conclusion that the last data pointed is null
+ *
+ * The first node is called head
+ * The last node is called tail
+ */
