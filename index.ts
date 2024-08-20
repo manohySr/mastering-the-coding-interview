2c19e9f18559830931e3d5655803315b81c935a1
manohySr <manohyrajaonah@gmail.com>
2024-08-20 22:51:43 +0400

    chore: why not using array ?

--- index.ts
+++ index.ts
@@ -6,13 +6,12 @@
 /**
  * Stack
  * It uses LIFO or Last In First Out
- * ex: 
+ * ex:
  *  context: making something top of each over and we can peek the topest of it
  *  History: undo | redo
  * Action that can be done:
  *  lookup O(n) - pop O(1) - push O(1) - peek O(1)
-*/
-
+ */
 
 /**
  * Queues
@@ -20,4 +19,8 @@
  * ex: waitlist example
  * Action that can be done:
  *  lookup O(n) - enqueue O(1) - dequeue O(1) - peek O(1)
+ */
+
+/**
+ * Why not using array for building queues DS? cause it's inefficient => like when we do unshift in array for adding an item - O(n)
  */
