344baff4b24dbff77af6a4384c9d26dea5984979
manohySr <manohyrajaonah@gmail.com>
2024-08-20 22:46:08 +0400

    add: 	description of stacks DS

---
+++ index.ts
@@ -0,0 +1,14 @@
+/**
+ * Stacks and queues
+ * Less method than over DS - less action to perform
+ */
+
+/**
+ * Stack
+ * It uses LIFO or Last In First Out
+ * ex: 
+ *  context: making something top of each over and we can peek the topest of it
+ *  History: undo | redo
+ * Action that can be do:
+ *  lookup O(n) - pop O(1) - push O(1) - peek O(1)
+*/
