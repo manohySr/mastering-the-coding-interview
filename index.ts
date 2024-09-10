6ca70d76bfefa878dc16f6a6d5da12a261c5cbaf
manohySr <manohyrajaonah@gmail.com>
2024-09-10 22:53:37 +0400

    wip: 	binary tree DS implementation

---
+++ index.ts
@@ -0,0 +1,16 @@
+
+// There are many tree DS
+// LinkedList are type of Tree
+// binary tree is one of the type
+// binary tree is a tree where a parent has only one or 2 childs
+
+class BinaryTree {
+  private value: any
+  private left: any
+  private right: any
+  constructor(value: any) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
